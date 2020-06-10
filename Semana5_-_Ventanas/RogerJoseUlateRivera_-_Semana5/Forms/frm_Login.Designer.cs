f0638f26401c3cd2e176aa63504c97a1fc690319
Roger Jose Ulate Rivera <roger.p.designe357@gmail.com>
2020-06-10 00:51:15 -0600

    nada importante

--- Semana5_-_Ventanas/RogerJoseUlateRivera_-_Semana5/Forms/frm_Login.Designer.cs
+++ Semana5_-_Ventanas/RogerJoseUlateRivera_-_Semana5/Forms/frm_Login.Designer.cs
@@ -28,12 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_ingresar = new System.Windows.Forms.Button();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.lbl_Usuario = new System.Windows.Forms.Label();
+            this.lbl_contrasenia = new System.Windows.Forms.Label();
+            this.txt_Usuario = new System.Windows.Forms.TextBox();
+            this.txt_contrasenia = new System.Windows.Forms.TextBox();
+            this.lbl_Bienvenidos = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_ingresar
+            // 
+            this.btn_ingresar.Location = new System.Drawing.Point(48, 171);
+            this.btn_ingresar.Name = "btn_ingresar";
+            this.btn_ingresar.Size = new System.Drawing.Size(75, 23);
+            this.btn_ingresar.TabIndex = 0;
+            this.btn_ingresar.Text = "Ingresar";
+            this.btn_ingresar.UseVisualStyleBackColor = true;
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Location = new System.Drawing.Point(152, 171);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(75, 23);
+            this.btn_salir.TabIndex = 1;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            // 
+            // lbl_Usuario
+            // 
+            this.lbl_Usuario.AutoSize = true;
+            this.lbl_Usuario.Location = new System.Drawing.Point(48, 90);
+            this.lbl_Usuario.Name = "lbl_Usuario";
+            this.lbl_Usuario.Size = new System.Drawing.Size(43, 13);
+            this.lbl_Usuario.TabIndex = 2;
+            this.lbl_Usuario.Text = "Usuario";
+            // 
+            // lbl_contrasenia
+            // 
+            this.lbl_contrasenia.AutoSize = true;
+            this.lbl_contrasenia.Location = new System.Drawing.Point(48, 127);
+            this.lbl_contrasenia.Name = "lbl_contrasenia";
+            this.lbl_contrasenia.Size = new System.Drawing.Size(61, 13);
+            this.lbl_contrasenia.TabIndex = 3;
+            this.lbl_contrasenia.Text = "Contraseña";
+            this.lbl_contrasenia.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txt_Usuario
+            // 
+            this.txt_Usuario.Location = new System.Drawing.Point(127, 90);
+            this.txt_Usuario.Name = "txt_Usuario";
+            this.txt_Usuario.Size = new System.Drawing.Size(100, 20);
+            this.txt_Usuario.TabIndex = 4;
+            // 
+            // txt_contrasenia
+            // 
+            this.txt_contrasenia.Location = new System.Drawing.Point(127, 127);
+            this.txt_contrasenia.Name = "txt_contrasenia";
+            this.txt_contrasenia.Size = new System.Drawing.Size(100, 20);
+            this.txt_contrasenia.TabIndex = 5;
+            // 
+            // lbl_Bienvenidos
+            // 
+            this.lbl_Bienvenidos.AutoSize = true;
+            this.lbl_Bienvenidos.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Bienvenidos.Location = new System.Drawing.Point(85, 34);
+            this.lbl_Bienvenidos.Name = "lbl_Bienvenidos";
+            this.lbl_Bienvenidos.Size = new System.Drawing.Size(114, 24);
+            this.lbl_Bienvenidos.TabIndex = 6;
+            this.lbl_Bienvenidos.Text = "Bienvenidos";
+            this.lbl_Bienvenidos.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // frm_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(276, 230);
+            this.Controls.Add(this.lbl_Bienvenidos);
+            this.Controls.Add(this.txt_contrasenia);
+            this.Controls.Add(this.txt_Usuario);
+            this.Controls.Add(this.lbl_contrasenia);
+            this.Controls.Add(this.lbl_Usuario);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.btn_ingresar);
+            this.Name = "frm_Login";
             this.Text = "frm_Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_ingresar;
+        private System.Windows.Forms.Button btn_salir;
+        private System.Windows.Forms.Label lbl_Usuario;
+        private System.Windows.Forms.Label lbl_contrasenia;
+        private System.Windows.Forms.TextBox txt_Usuario;
+        private System.Windows.Forms.TextBox txt_contrasenia;
+        private System.Windows.Forms.Label lbl_Bienvenidos;
     }
 }
--- Semana5_-_Ventanas/RogerJoseUlateRivera_-_Semana5/Forms/frm_Login.cs
+++ Semana5_-_Ventanas/RogerJoseUlateRivera_-_Semana5/Forms/frm_Login.cs
@@ -16,5 +16,15 @@ namespace RogerJoseUlateRivera___Semana5.Forms
         {
             InitializeComponent();
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
